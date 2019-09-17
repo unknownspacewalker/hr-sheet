9dfdb8c4358483aa97545da83b267105223b2beb
Vladimir Tkachenko <vtkachenko@griddynamics.com>
2019-09-17 16:52:29 +0300

    return pure data instead of axios response from getAllActive function

---
+++ src/interfaces/employee.interface.ts
@@ -0,0 +1,37 @@
+interface IEmployee {
+  readonly id: number;
+  general: {
+    readonly username: string | null;
+    readonly firstName: string | null;
+    readonly familyName: string | null;
+    readonly nativeFullName: string | null;
+    readonly birthday: string | null;
+  }
+  jobInfo: {
+    readonly hiringDate: string | null;
+    readonly managerId: string | null;
+  },
+  bamboo: {
+    readonly id: number;
+    readonly firstName: string | null;
+    readonly familyName: string | null;
+    readonly gender: string | null;
+  },
+  latestGrade: {
+    readonly location: string | null;
+    readonly position: string | null;
+    readonly track: string | null;
+    readonly level: number;
+    readonly workProfile: string | null;
+    readonly specialization: string | null;
+  },
+  social: {
+    readonly email: string | null;
+    readonly phone: string | null;
+    readonly internalPhone: string | null;
+    readonly socialCastContact: string | null;
+    readonly gitHubContact: string | null;
+    readonly dockerCloudId: string | null;
+    readonly skypeId: string | null;
+  }
+}
--- src/pmo/api/employees/getAllActive.ts
+++ src/pmo/api/employees/getAllActive.ts
@@ -5,13 +5,14 @@ dotenv.config();
 
 const url = 'http://internaltools.pmo.aws.griddynamics.net:8080/service/api/employee/active';
 
-function getAllActive(jsessionid: string) {
-  return axios(url, {
+function getAllActive(jsessionid: string): Promise<IEmployee[]> {
+  return axios.get<IEmployee[]>(url, {
     headers: {
       'Cookie': `JSESSIONID=${jsessionid}`,
       'Content-Type': 'application/json',
     },
-  });
+  })
+    .then((response) => response.data);
 }
 
 export default getAllActive;
