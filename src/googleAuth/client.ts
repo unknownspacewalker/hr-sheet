ed31a1fc32e8aca2421427ecb976c459346aa6e5
Vladimir Ulianov <vulianov@griddynamics.com>
2019-09-18 17:34:47 +0300

    work with sheet

--- src/googleAuth/client.ts
+++ src/googleAuth/client.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import { fromFile } from "./clientFactory";
 
-export default fromFile("google-key.json");
+export default fromFile(path.resolve(process.cwd(), "google-key.json"));
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
-import signIn from './pmo/api/auth/signIn';
-import getAllActive from './pmo/api/employees/getAllActive';
+import signIn from "./pmo/api/auth/signIn";
+import getAllActive from "./pmo/api/employees/getAllActive";
+
+import sheet from "./sheet";
 
 (async () => {
   try {
-    const jsessionid = await signIn();
-    console.log(await getAllActive(jsessionid));
+    // const jsessionid = await signIn();
+    // console.log(await getAllActive(jsessionid));
+
+    await sheet();
   } catch (e) {
-    console.log('error:', e.message);
+    console.log("error:", e.message);
+    console.log("stack:", e.stack);
   }
 })();
