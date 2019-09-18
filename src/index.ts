845845c93348c2360d996b7fe3f8181a56c052e7
DariGal <d.galichkina@gmail.com>
2019-09-18 19:53:39 +0300

    added projects to employees

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import signIn from './pmo/api/auth/signIn';
 import getAllActive from './pmo/api/employees/getAllActive';
+import getEmployeeProjects from './pmo/api/employees/getEmployeeProjects';
 
 (async () => {
   try {
     const jsessionid = await signIn();
-    console.log(await getAllActive(jsessionid));
+    const allEmployees = await getAllActive(jsessionid);
+    const uiEngineers = await Promise.all(allEmployees
+      .filter((employee) => employee.latestGrade.specialization === 'UI')
+      .map(async (uiEngineer) => ({
+        ...uiEngineer,
+        projects: await getEmployeeProjects(uiEngineer.general.username, jsessionid),
+      })));
+
+    console.log(uiEngineers);
   } catch (e) {
     console.log('error:', e.message);
   }
