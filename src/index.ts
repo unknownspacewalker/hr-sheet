8990a8b7a01cca23b601ef2d4513e288f05af2d2
Vladimir Tkachenko <vtkachenko@griddynamics.com>
2019-09-19 14:02:14 +0300

    added loader with status

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,37 @@
+import ora from 'ora';
+
 import signIn from './pmo/api/auth/signIn';
 import getAllActive from './pmo/api/employees/getAllActive';
 import getEmployeeProjects from './pmo/api/employees/getEmployeeProjects';
 
+
 (async () => {
   try {
+    const PMOAuthSpinner = ora('Authorization in PMO').start();
     const jsessionid = await signIn();
-    const allEmployees = await getAllActive(jsessionid);
-    const uiEngineers = await Promise.all(allEmployees
-      .filter((employee) => employee.latestGrade.specialization === 'UI')
-      .map(async (uiEngineer) => ({
-        ...uiEngineer,
-        projects: await getEmployeeProjects(uiEngineer.general.username, jsessionid),
-      })));
+    PMOAuthSpinner.succeed();
 
-    console.log(uiEngineers);
+    const PMOGetEmployees = ora('Fetching PMO employee list').start();
+    const allEmployees = await getAllActive(jsessionid);
+    PMOGetEmployees.succeed();
+
+    const UIRawEngeneers = allEmployees
+      .filter((employee) => employee.latestGrade.specialization === 'UI');
+
+    const PMOGetEmployeesProjects = ora(`Fetching employees' projects [0/${UIRawEngeneers.length}]`).start();
+    let counter = 0;
+    const incCounter = () => {
+      counter += 1;
+      PMOGetEmployeesProjects.text = `Fetching UI engineers' projects [${counter}/${UIRawEngeneers.length}]`;
+    };
+    const uiEngineers = await Promise.all(UIRawEngeneers
+      .map(async (uiEngineer) => ({
+        uiEngineer,
+        projects: await getEmployeeProjects(uiEngineer.general.username, jsessionid, incCounter),
+      })));
+    PMOGetEmployeesProjects.succeed();
+
+    console.log(uiEngineers.length);
   } catch (e) {
     console.log('error:', e.message);
   }
