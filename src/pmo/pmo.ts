1a04196527569492459a20496ffa945db6e4f56b
Vladimir Tkachenko <vtkachenko@griddynamics.com>
2019-10-07 13:30:05 +0300

    mocked google factory sync

--- src/pmo/pmo.ts
+++ src/pmo/pmo.ts
@@ -1,44 +1,49 @@
-import signIn from "./api/auth/signIn";
-import getAllActive from "./api/employees/getAllActive";
-import getEmployeeProjects from "./api/employees/getEmployeeProjects";
-import getPriority from "./utils/getPriority";
-import promiseAllWithBandWidth from "./utils/promiseAllWithBandWidth";
-import IEmployee from "../interfaces/IEmployee";
+import signIn from './api/auth/signIn';
+import getAllActive from './api/employees/getAllActive';
+import getEmployeeProjects from './api/employees/getEmployeeProjects';
+import getPriority from './utils/getPriority';
+import promiseAllWithBandWidth from './utils/promiseAllWithBandWidth';
+import IEmployee from '../interfaces/IEmployee';
+
+type Props = {
+  specialization?: 'UI' | 'HR'
+};
 
 class PMO {
+  specialization: string;
+
   jsessionid: string;
 
   rawUIEngineers: IGetAllActiveResponse;
 
   UIEngineers: IEmployee[];
 
-  constructor() {
+  constructor({ specialization = 'UI' }: Props = {}) {
+    this.specialization = specialization;
     this.jsessionid = undefined;
     this.rawUIEngineers = undefined;
     this.UIEngineers = undefined;
   }
 
-  auth = async function() {
+  auth = async function () {
     this.jsessionid = await signIn();
   };
 
-  getActiveUIEmployees = async function() {
+  getActiveUIEmployees = async function () {
     const rawEmployees = await getAllActive(this.jsessionid);
     this.rawUIEngineers = rawEmployees
-      .filter(employee => employee.latestGrade.specialization === "UI")
+      .filter((employee) => employee.latestGrade.specialization === this.specialization)
       .slice(0, 25);
   };
 
-  getAccountType = async function() {
-    console.log("get account types");
+  getAccountType = async function () {
+    console.log('get account types');
   };
 
-  getUIEngineers = async function(callback: () => void) {
+  getUIEngineers = async function (callback: () => void) {
     this.UIEngineers = await promiseAllWithBandWidth(
-      this.rawUIEngineers.map(
-        (
-          employee: IGetAllActiveResponseItem
-        ): (() => Promise<IEmployee>) => async () => ({
+      this.rawUIEngineers
+        .map((employee: IGetAllActiveResponseItem): (() => Promise<IEmployee>) => async () => ({
           id: employee.id,
           manager: employee.jobInfo.managerId,
           username: employee.general.username,
@@ -51,11 +56,10 @@ class PMO {
             await getEmployeeProjects(
               employee.general.username,
               this.jsessionid,
-              callback
-            )
-          )
-        })
-      )
+              callback,
+            ),
+          ),
+        })),
     );
   };
 }
