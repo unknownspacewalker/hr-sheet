33a6f91a2baae29ec849a6798297c629b3c0a101
Vladimir Ulianov <vulianov@griddynamics.com>
2019-10-08 16:12:23 +0300

    google willingness sort

--- src/google/hrProcessorFactory/compareSheetRowData.ts
+++ src/google/hrProcessorFactory/compareSheetRowData.ts
@@ -3,6 +3,9 @@ import ISheetRowData from './ISheetRowData';
 const comparePriorityAsc = (
   firstData: ISheetRowData,
   secondData: ISheetRowData,
-): number => firstData.Priority - secondData.Priority;
+): number => (
+  (firstData.Priority - secondData.Priority)
+    || (Number(firstData.Willingness) - Number(secondData.Willingness))
+);
 
 export default comparePriorityAsc;
