0be099e1707d00cf044de89a490f72df4795da80
Vladimir Ulianov <vulianov@griddynamics.com>
2019-12-14 09:40:14 -0800

    remove useless comments, added return value

--- handler.ts
+++ handler.ts
@@ -1,6 +1,6 @@
 import 'source-map-support/register';
 import { syncronizeSheet } from './src';
 
-export const sync = async () => {
+export const sync = async (): Promise<void> => {
   await syncronizeSheet();
 };
